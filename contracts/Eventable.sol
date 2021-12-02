ea9051433d3c1fdc768257b50c39f255e9418f8a
Sergey Sharshunov <s.sharshunov@gmail.com>
2021-12-02 13:35:37 +0500

    Linted EventLifeCycle.sol, Eventable.sol, GovernanceToken.sol, TokenTemplate, PoolToken.sol, IPoolToken.SOL

--- contracts/Eventable.sol
+++ contracts/Eventable.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.7.4;
+
 // "SPDX-License-Identifier: MIT"
 
 interface Eventable {
-    
-    function submitEventStarted(uint256 currentEventPriceChangePercent) external; 
+    function submitEventStarted(uint256 currentEventPriceChangePercent)
+        external;
+
     function submitEventResult(int8 _result) external;
-    
 }
