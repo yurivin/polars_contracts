f841daa1ca1539497be946ea5c819c08c3b936cc
Юрий <discours@list.ru>
2022-02-17 09:45:08 +0300

    Revert "Revert "Predicition pool fees distribution adjustment""
    This reverts commit 3a2201e39b526dde8481878c8fd19779a88d4b8e.

--- contracts/PredictionPool.sol
+++ contracts/PredictionPool.sol
@@ -542,7 +542,7 @@ contract PredictionPool is Eventable, DSMath {
                 _blackPrice,
                 minPrice,
                 tokensAmount,
-                false
+                true
             );
         _blackBought = _blackBought.sub(tokensAmount);
         _collateralForBlack = _collateralForBlack.sub(collateralAmountWithFee);
@@ -562,7 +562,7 @@ contract PredictionPool is Eventable, DSMath {
         (
             uint256 collateralAmountWithFee,
             uint256 collateralToSend
-        ) = genericSell(_whiteToken, _whitePrice, minPrice, tokensAmount, true);
+        ) = genericSell(_whiteToken, _whitePrice, minPrice, tokensAmount, false);
         _whiteBought = _whiteBought.sub(tokensAmount);
         _collateralForWhite = _collateralForWhite.sub(collateralAmountWithFee);
         _whiteSoldThisCycle = _whiteSoldThisCycle.add(tokensAmount);
