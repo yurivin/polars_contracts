33a20c45ac1a4c4152cd87b42653e786f1e55bef
Sergey Sharshunov <s.sharshunov@gmail.com>
2022-07-08 12:53:50 +0500

    Fix revert message in PendingOrders (ACCOUNT HAS NO ORDERS) and new MAX_ORDERS_ALLOWED functionality

--- contracts/IPendingOrders.sol
+++ contracts/IPendingOrders.sol
@@ -6,4 +6,18 @@ interface IPendingOrders {
     function eventStart(uint256 _eventId) external;
 
     function eventEnd(uint256 _eventId) external;
+
+    function createOrder(
+        uint256 _amount,
+        bool _isWhite,
+        uint256 _eventId
+    ) external;
+
+    function cancelOrder(uint256 orderId) external;
+
+    function _eventContractAddress() external view returns (address);
+
+    function _predictionPool() external view returns (address);
+
+    function withdrawCollateral() external returns (uint256);
 }
